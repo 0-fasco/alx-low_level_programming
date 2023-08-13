0a7714f8d5d89733fca673999844ee509a0cffd5
Oluwasegun Adedeji <shegzy5009@gmail.com>
2023-08-13 03:23:40 -0700

    gfhgf

---
+++ 0x01-variables_if_else_while/qwe/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit number
+ * of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
